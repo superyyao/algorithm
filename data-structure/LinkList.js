b93e2b4a6a2c159811ec76d259d19dbe8a8e61dc
superyyao <superyyao@github.com>
2024-06-26 21:24:53 +0800

    add check link cycle and remove link element

--- data-structure/LinkList.js
+++ data-structure/LinkList.js
@@ -39,6 +39,66 @@ class LinkNodeList {
             console.log("empty")
         }
     }
+    addCycle(val) {
+        let p = this.head
+        let ele = null
+        while (p.next) {
+            if (val === p.val) {
+                ele = p
+            }
+            p = p.next
+        }
+        if (ele) {
+            p.next = ele
+        }
+    }
+    // 移除某个元素
+    removeElements(val) {
+        // 利用哨兵 => 1 =>2 => 3
+        let ele = {
+            next: this.head
+        }
+        let p = ele
+        while(p.next) {
+            if (p.next.val=== val) {
+                p.next= p.next.next
+            } else {
+                p = p.next
+            }
+        }
+        // 返回新的链表
+        this.head = ele.next
+    }
+
+    // 检查回环
+    hasCycle() {
+        // let cache = new Set()
+        // let p = this.head
+        // // 检查回环
+        // while (p) {
+        //     if (cache.has(p)) {
+        //         return true
+        //     } else {
+        //         cache.add(p)
+        //     }
+        //     p = p.next
+        // }
+        // return false
+
+
+        // 双指针检查回环
+        let fast = this.head
+        let slow = this.head
+        while(fast && fast.next) {
+            fast = fast.next.next
+            slow = slow.next
+            
+            if (slow === fast) {
+                return true
+            }
+        }
+        return false
+    }
 }
 
 let linkList = new LinkNodeList()
@@ -46,6 +106,15 @@ linkList.append(1)
 linkList.append(2)
 linkList.append(3)
 linkList.append(4)
+
 console.log(linkList.length)
 console.log(linkList.print())
 console.log(linkList)
+
+//linkList.removeElements(1)
+//console.log(linkList.print())
+
+// 定义回环
+linkList.addCycle(2)
+console.log(linkList.hasCycle())
+
